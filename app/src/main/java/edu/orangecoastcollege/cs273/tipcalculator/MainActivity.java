7e4fd840b83abd8ceeeebb86b025851f3a040fe3
jkloppenburg1 <jkloppenburg1@student.cccd.edu>
2016-09-13 16:08:02 -0700

    Application completed

--- app/src/main/java/edu/orangecoastcollege/cs273/tipcalculator/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/tipcalculator/MainActivity.java
@@ -8,15 +8,22 @@ import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    private static NumberFormat currency = NumberFormat.getCurrencyInstance();
+    private static NumberFormat percent = NumberFormat.getPercentInstance();
+
 
     // Associate the controller with the needed views
     private EditText amountEditText;
     private TextView amountTextView;
     private TextView percentTextView;
-    private TextView tipTextView;
+   // private TextView tipTextView;
     private TextView tipAmountTextView;
-    private TextView totalTextView;
+    //private TextView totalTextView;
     private TextView totalAmountTextView;
     private SeekBar percentSeekBar;
 
@@ -31,14 +38,17 @@ public class MainActivity extends AppCompatActivity {
         amountEditText = (EditText) findViewById(R.id.amountEditText);
         amountTextView = (TextView) findViewById(R.id.amountTextView);
         percentTextView = (TextView) findViewById(R.id.percentTextView);
-        tipTextView = (TextView) findViewById(R.id.tipTextView);
+        //tipTextView = (TextView) findViewById(R.id.tipTextView);
         tipAmountTextView = (TextView) findViewById(R.id.tipAmountTextView);
-        totalTextView = (TextView) findViewById(R.id.totalTextView);
+        //totalTextView = (TextView) findViewById(R.id.totalTextView);
         totalAmountTextView = (TextView) findViewById(R.id.totalAmountTextView);
         percentSeekBar = (SeekBar) findViewById(R.id.percentSeekBar);
 
         //Define a listener for the amountEditText (onTextChanged)
         amountEditText.addTextChangedListener(amountTextChangedListener);
+
+        // Define a listener for our percentSeekBar (onProgressChanged);
+        percentSeekBar.setOnSeekBarChangeListener(percentChangedListener);
     }
 
     private TextWatcher amountTextChangedListener = new TextWatcher() {
@@ -52,18 +62,68 @@ public class MainActivity extends AppCompatActivity {
             //Try to get the amount from amountEditText
             try
             {
-                double amount = Double.parseDouble(charSequence.toString()) / 100;
-                currentBill.setAmount(amount);
+                if (charSequence.length() > 0) {
+                    double amount = Double.parseDouble(charSequence.toString()) / 100.0;
+                    currentBill.setAmount(amount);
+                }
+                else
+                {
+                    double amount = 0.0;
+                    currentBill.setAmount(amount);
+                }
+
             }
             catch (NumberFormatException e)
             {
                 amountEditText.setText("");
             }
+
+            //No exception, input is valid:
+
+            updateViews();
         }
 
         @Override
-        public void afterTextChanged(Editable editable) {
+        public void afterTextChanged(Editable editable)
+        {
+
 
         }
     };
+
+    private SeekBar.OnSeekBarChangeListener percentChangedListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+            //Update the model with the new tip amount
+            currentBill.setTipPercent(i / 100.0);
+
+            // Update the percentTextView
+            percentTextView.setText(percent.format(currentBill.getTipPercent()));
+
+            //Update the views
+            updateViews();
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
+
+    private void updateViews()
+    {
+        // 1) Set the bill amount
+        amountTextView.setText(currency.format(currentBill.getAmount()));
+
+        // 2) Set the tip amount (tipTextView)
+        tipAmountTextView.setText(currency.format(currentBill.getTipAmount()));
+
+        //3
+        totalAmountTextView.setText(currency.format(currentBill.getTotalAmount()));
+    }
 }
